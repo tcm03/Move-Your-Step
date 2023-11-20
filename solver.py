b4e41e86b829146903d3d86ed64aab5f8c8b6c98
tcm03 <149855387+tcm03@users.noreply.github.com>
2023-11-21 00:44:56 +0700

    Create solver.py

---
+++ solver.py
@@ -0,0 +1,88 @@
+from queue import Queue
+from custom_parser import *
+import sys
+
+INF = 1000000000
+
+def breadth_first_search(school_map):
+    """
+    Description:
+        Find the shortest path from the start to the goal using breadth-first search.
+    Args:
+        school_map: a 2D array of strings, representing the school map
+    Returns:
+        a tuple (d, path), where d is the shortest path length and path is a list of coordinates
+        [(x1, y1), (x2, y2), ..., (xk, yk)] such that (x1, y1) is the start, (xk, yk) is the goal,
+        and (xi, yi) and (xi+1, yi+1) are adjacent cells in the path.
+        If there is no path, return None.
+    """
+    N = len(school_map)
+    M = len(school_map[0])
+    for i in range(N):
+        for j in range(M):
+            if school_map[i][j][0] == "A":
+                start = (i, j)
+    Q = Queue()
+    Q.put(start)
+    trace = []
+    for i in range(N):
+        arr = []
+        for j in range(M):
+            arr.append((-1, -1))
+        trace.append(arr)
+    dist = []
+    for i in range(N):
+        arr = []
+        for j in range(M):
+            arr.append(INF)
+        dist.append(arr)
+    dist[start[0]][start[1]] = 0
+    goal = (-1, -1)
+    while Q.qsize() > 0:
+        if goal != (-1, -1):
+            break
+        x, y = Q.get()
+        for x_offset, y_offset in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            next_x, next_y = x + x_offset, y + y_offset
+            if next_x < 0 or next_x >= N or next_y < 0 or next_y >= M:
+                continue
+            if school_map[next_x][next_y] == "-1":
+                continue
+            if dist[next_x][next_y] != INF:
+                continue
+            dist[next_x][next_y] = dist[x][y] + 1
+            trace[next_x][next_y] = (x, y)
+            if school_map[next_x][next_y][0] == "T":
+                goal = (next_x, next_y)
+                break
+            Q.put((next_x, next_y))
+    if goal == (-1, -1):
+        return None
+    else:
+        d = dist[goal[0]][goal[1]]
+        path = []
+        while goal != (-1, -1):
+            path.append(goal)
+            goal = trace[goal[0]][goal[1]]
+        path.reverse()
+        return d, path
+
+
+def main():
+    if len(sys.argv) < 2:
+        sys.exit("Please enter the input file name.")
+    elif len(sys.argv) > 2:
+        sys.exit("Too many arguments.")
+    school_map = read_input(sys.argv[1])
+    answer = breadth_first_search(school_map)
+    if answer:
+        d, path = answer
+        print(f"Shortest path length: {d}")
+        print("Path:")
+        for x, y in path:
+            print(f"({x}, {y})")
+    else:
+        print("No solution.")
+
+if __name__ == "__main__":
+    main()
