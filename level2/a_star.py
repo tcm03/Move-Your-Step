1ac971b3f6725fc19b97fb715553f062acd5629b
phucst123 <huuphucst123@gmail.com>
2023-11-29 16:05:40 +0700

    add a_star

---
+++ level2/a_star.py
@@ -0,0 +1,156 @@
+import math
+import sys
+from queue import PriorityQueue
+from custom_parser import *
+
+
+INF = 1000000000
+
+
+def evaluate_euclidean_distance(current, goal):
+    return math.sqrt((current[0]-goal[0])**2 + (current[1]-goal[1])**2)
+
+
+def next_move(school_map, current):
+    N = len(school_map)  # number of rows
+    M = len(school_map[0])  # number of columns
+
+    current_x = current[0]
+    current_y = current[1]
+    current_key_set = current[2]
+    available_moves = []
+
+    for x_offset, y_offset in [(0, 1), (1, 0), (0, -1), (-1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]:
+        next_x, next_y, next_key_set = current_x + x_offset, current_y + y_offset, current_key_set
+        # invalid next cell
+        if next_x < 0 or next_x >= N or next_y < 0 or next_y >= M:
+            continue
+
+        if school_map[next_x][next_y] == "-1":
+            continue
+
+        if abs(x_offset + y_offset) != 1:
+            # check if neighboring cells are walls
+            if school_map[current_x + x_offset][current_y + 0] == '-1':
+                continue
+            if school_map[current_x + 0][current_y + y_offset] == '-1':
+                continue
+
+        if school_map[next_x][next_y][0] == "D":
+            # check if the key is available
+            key_number = int(school_map[next_x][next_y][1])
+            # check whether agent has corresponding key
+            if current_key_set & (1 << (key_number - 1)) == 0:
+                continue
+
+        if school_map[next_x][next_y][0] == "K":
+            key_number = int(school_map[next_x][next_y][1])
+            next_key_set |= (1 << (key_number - 1))
+
+        available_moves.append((next_x, next_y, next_key_set))
+
+    return available_moves
+
+
+def a_star(school_map):
+    N = len(school_map)  # number of rows
+    M = len(school_map[0])  # number of columns
+
+    start = (0, 0, 0)
+    goal = (0, 0, 0)
+
+    num_keys = 0
+    for i in range(N):
+        for j in range(M):
+            if school_map[i][j][0] == "K":
+                num_keys = max(num_keys, int(school_map[i][j][1:]))
+    K = 1 << num_keys
+
+    for i in range(N):
+        for j in range(M):
+            if school_map[i][j][0] == "A":
+                start = (i, j, 0)
+            if school_map[i][j][0] == "T":
+                goal = (i, j, 0)  # start cell
+
+    trace = []
+    for i in range(N):
+        arr = []
+        for j in range(M):
+            sub_arr = []
+            for k in range(K):
+                sub_arr.append((-1, -1, -1))
+            arr.append(sub_arr)
+        trace.append(arr)
+    dist = []
+    for i in range(N):
+        arr = []
+        for j in range(M):
+            sub_arr = []
+            for k in range(K):
+                sub_arr.append(INF)
+            arr.append(sub_arr)
+        dist.append(arr)
+    dist[start[0]][start[1]][start[2]] = 0
+
+    frontier = PriorityQueue()
+
+    # add starting node into frontier
+    frontier.put((evaluate_euclidean_distance(current=start, goal=goal), start))
+
+    while not frontier.empty():
+
+        f_value, (x, y, key_set) = frontier.get()
+
+        # check cell is goal
+        if (x, y) == (goal[0], goal[1]):
+            d = dist[x][y][key_set]
+            path = []
+            while (x, y, key_set) != (-1, -1, -1):
+                key_set_str = bin(key_set)[2:].zfill(num_keys)[::-1]
+                path.append((x, y, key_set_str))
+                (x, y, key_set) = trace[x][y][key_set]
+            path.reverse()
+            return d, path
+
+        available_moves = next_move(current=(x, y, key_set), school_map=school_map)
+
+        for next_x, next_y, next_key_set in available_moves:
+            # next cell is already visited
+            if dist[next_x][next_y][next_key_set] != INF:
+                continue
+
+            # update path and heuristic
+            dist_next = dist[x][y][key_set] + 1
+            heuristic_next = evaluate_euclidean_distance(current=(next_x, next_y), goal=goal)
+            dist[next_x][next_y][next_key_set] = dist_next
+            trace[next_x][next_y][next_key_set] = (x, y, key_set)
+
+            #
+            # check cell exists in frontier and update if needed
+            #
+
+            frontier.put((dist_next+heuristic_next, (next_x, next_y, next_key_set)))
+
+    return None, None
+
+
+def main():
+    if len(sys.argv) < 2:
+        sys.exit("Please enter the input file name.")
+    elif len(sys.argv) > 2:
+        sys.exit("Too many arguments.")
+    school_map = read_input(sys.argv[1])
+    answer = a_star(school_map)
+    if answer:
+        d, path = answer
+        print(f"Shortest path length: {d}")
+        print("Path:")
+        for x, y in path:
+            print(f"({x}, {y})")
+    else:
+        print("No solution.")
+
+
+if __name__ == "__main__":
+    main()
